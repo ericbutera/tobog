23da2d2e31b5d13c2ab21dadc4f1ca700f5f287b
er1c_ <eric.butera@gmail.com>
2013-11-06 20:23:44 -0500

    merging in privmsg parsing

--- privmsg.go
+++ privmsg.go
@@ -11,41 +11,68 @@ func main() {
   m["action"]= "PRIVMSG #channel :\u0001ACTION lorem ipsum\u0001"
   m["version"] = "PRIVMSG nick :\u0001VERSION\u0001"
   m["chanversion"] = "PRIVMSG #chan :\u0001VERSION\u0001"
+  m["001"] = "001 moo511 :Welcome to the debian Internet Relay Chat Network moo511"
+  m["notice"] = "NOTICE moo511 :*** You are exempt from user limits. congrats."
+  m["join"] = "JOIN :#MOO"
+  m["quit"] = "QUIT :Quit: leaving"
+  m["mode"] = "MODE moo511 :+i"
+
   for key, val := range m {
     fmt.Printf("key %v val [%v]\n", key, val)
 
-    colon := strings.Index(val, ":")
-    actionChar := "\u0001"
-    startNickOffset := 8
-    endNickOffset := colon - 1
-    nick := val[startNickOffset:endNickOffset]
-    fmt.Printf("endNickOffset %d\n", endNickOffset)
-    fmt.Printf("nick [%s]\n", nick)
+    // figure out command PRIVMSG NOTICE 001 JOIN
+    cmdOffset := strings.Index(val, " ")
+    fmt.Printf("cmdOffset %d\n", cmdOffset)
 
-    fmt.Printf("raw [%v]\n", []byte(val))
-    if val[colon+1:colon+2] == actionChar {
-      fmt.Printf("ctcp or action\n")
-      if val[colon+2:colon+8] == "ACTION" {
-        msg := val[colon+9: len(val)-1]
-        fmt.Printf("action!\n")
-        fmt.Printf("msg [%v]\n", msg)
-        fmt.Printf("msg [%v]\n", []byte(msg))
-      } else {
-        msg := val[colon+2: len(val)-1]
-        fmt.Printf("action!\n")
-        fmt.Printf("msg [%v]\n", msg)
-        fmt.Printf("msg [%v]\n", []byte(msg))
+    if cmdOffset > 0 {
+      cmd := val[0:cmdOffset]
+      fmt.Printf("cmd [%v]\n", cmd)
+
+      colon := strings.Index(val, ":")
+      actionChar := "\u0001"
+
+      switch cmd {
+        case "PRIVMSG":
+          startToOffset := 8
+          endToOffset := colon - 1
+          to := val[startToOffset:endToOffset] //Message.To
+          fmt.Printf("endToOffset %d\n", endToOffset)
+          fmt.Printf("to [%s]\n", to)
+
+          fmt.Printf("raw [%v]\n", []byte(val))
+          if val[colon+1:colon+2] == actionChar {
+            fmt.Printf("ctcp or action\n")
+            if len(val) > colon+9 && val[colon+2:colon+8] == "ACTION" {
+              msg := val[colon+9: len(val)-1]
+              fmt.Printf("action!\n")
+              fmt.Printf("msg [%v]\n", msg)
+              fmt.Printf("msg [%v]\n", []byte(msg))
+            } else {
+              msg := val[colon+2: len(val)-1]
+              fmt.Printf("action!\n")
+              fmt.Printf("msg [%v]\n", msg)
+              fmt.Printf("msg [%v]\n", []byte(msg))
+            }
+          } else {
+            fmt.Printf("message!\n")
+            msg := val[colon+1: len(val)]
+            fmt.Printf("msg [%v]\n", msg)
+            fmt.Printf("msg [%v]\n", []byte(msg))
+          }
+        /*case "NOTICE":
+          startToOffset := 7
+          endToOffset := colon - 1
+          to := val[startToOffset:endToOffset] //Message.To
+          fmt.Printf("endToOffset %d\n", endToOffset)
+          fmt.Printf("to [%s]\n", to)
+          msg := val[colon+1: len(val)]
+          fmt.Printf("msg [%v]\n", msg)
+          fmt.Printf("msg [%v]\n", []byte(msg))
+        */
       }
-    } else {
-      fmt.Printf("message!\n")
-      msg := val[colon+1: len(val)]
-      fmt.Printf("msg [%v]\n", msg)
-      fmt.Printf("msg [%v]\n", []byte(msg))
-    }
 
-    // 01234567
-    // PRIVMSG_TO_:MESSAGE
-    fmt.Printf("--------------------------\n")
+      fmt.Printf("--------------------------\n")
+    }
   }
 
 }
